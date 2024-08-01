0e6d1e744c33a6c4d5a78f265633fae362915a9e
Zekeriya <Zekeriya@ZEKERIYA>
2024-08-01 12:52:52 +0300

    Add project files.

---
+++ PratikPatikaKutuphane/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PratikPatikaKutuphane
+{
+    public class Book
+    {
+        public string authorName {  get; set; }
+        public string bookName { get; set; }
+        public string page { get; set; }
+        public string publisher { get; set; }
+        public DateTime registrationDate { get; set; }
+
+        //Varsayılan Constructor Metot (parametresiz)
+        public Book()
+        {
+            authorName = "Bilinmeyen Yazar";
+            bookName = "Bilinmeyen Kitap İsmi";
+            page = "Bilinmeyen Sayfa Sayısı";
+            publisher = "Bilinmeyen Yayınevi";
+            registrationDate = DateTime.Now;
+        }
+       // Parametreli Constructor Metot
+        public Book(string _authorName,string _bookName, string _page, string _publisher)
+        {
+            authorName = _authorName;
+            bookName = _bookName;
+            page = _page;
+            publisher = _publisher;
+            registrationDate = DateTime.Now;
+        }
+
+    }
+    
+}
---
+++ PratikPatikaKutuphane/Program.cs
@@ -0,0 +1,11 @@
+﻿using PratikPatikaKutuphane;
+
+Book book1 = new Book();
+
+//parametre gönderilmiyor
+Console.WriteLine($"{book1.bookName},{book1.authorName},{book1.page},{book1.publisher},{book1.registrationDate}");
+
+//parametre gönderiliyor
+Book book2 = new Book("Aylin Kulin","Adı Aylin","398","Remzi Kitabevi");
+
+Console.WriteLine($"{book2.bookName},{book2.authorName},{book2.page},{book2.publisher},{book2.registrationDate}");
